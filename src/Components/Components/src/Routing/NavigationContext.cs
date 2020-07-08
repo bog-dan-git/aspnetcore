4040c6f22e0a06217eb75250af466905948c68f6
Safia Abdalla <safia@safia.rocks>
2020-07-08 20:24:15 +0000

    Address feedback from peer review

--- src/Components/Components/src/Routing/NavigationContext.cs
+++ src/Components/Components/src/Routing/NavigationContext.cs
@@ -1,15 +1,14 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Components.Routing
 {
+    /// <summary>
+    /// Provides information about the current asynchronous navigation event
+    /// including the target path and the cancellation token.
+    /// </summary>
     public class NavigationContext
     {
         internal NavigationContext(string path, CancellationToken cancellationToken)
